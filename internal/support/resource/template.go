71ff47bdaedffd7b8dec589fee0b29a3be8a5f5c
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-11-21 14:27:14 -0300

    feat(resource): add handler functions for standard HTTP methods

---
+++ internal/support/resource/template.go
@@ -0,0 +1,87 @@
+package resource
+
+import (
+	"net/http"
+
+	"github.com/alan-b-lima/almodon/internal/auth"
+)
+
+func GetHandler[TRes any](gk gatekeeper, proc func(auth.Actor) (TRes, error), w http.ResponseWriter, r *http.Request) {
+	act, err := Session(gk, r)
+	if err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	res, err := proc(act)
+	if err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	if err := EncodeJSON(&res, http.StatusOK, w, r); err != nil {
+		WriteError(w, err)
+		return
+	}
+}
+
+func PostHandler[TRes, TReq any](gk gatekeeper, proc func(act auth.Actor, req TReq) (TRes, error), w http.ResponseWriter, r *http.Request) {
+	act, err := Session(gk, r)
+	if err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	var req TReq
+	if err := DecodeJSON(&req, r); err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	uuid, err := proc(act, req)
+	if err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	if err := EncodeJSON(&uuid, http.StatusCreated, w, r); err != nil {
+		WriteError(w, err)
+		return
+	}
+}
+
+func PutHandler[TReq any](gk gatekeeper, proc func(act auth.Actor, req TReq) error, w http.ResponseWriter, r *http.Request) {
+	act, err := Session(gk, r)
+	if err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	var req TReq
+	if err := DecodeJSON(&req, r); err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	if err := proc(act, req); err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func DeleteHandler(gk gatekeeper, proc func(act auth.Actor) error, w http.ResponseWriter, r *http.Request) {
+	act, err := Session(gk, r)
+	if err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	if err := proc(act); err != nil {
+		WriteError(w, err)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
