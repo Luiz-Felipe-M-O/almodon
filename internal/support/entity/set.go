546a2d382dc73ee4746318076a999e660c147b06
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-11-21 14:26:31 -0300

    feat(entity): add SomeThen function for optional value processing

--- internal/support/entity/set.go
+++ internal/support/entity/set.go
@@ -1,6 +1,10 @@
 package entity
 
-import "time"
+import (
+	"time"
+
+	"github.com/alan-b-lima/almodon/pkg/opt"
+)
 
 func Set[D, S any](dst *D, src S, proc func(S) (D, error)) error {
 	val, err := proc(src)
@@ -21,5 +25,20 @@ func SetWithUpdate[D, S any](dst *D, src S, proc func(S) (D, error), update *tim
 		*update = time.Now()
 	}
 
+	return nil
+}
+
+func SomeThen[F, R any](dst *opt.Opt[R], src opt.Opt[F], fn func(F) (R, error)) error {
+	val, ok := src.Unwrap()
+	if !ok {
+		return nil
+	}
+
+	res, err := fn(val)
+	if err != nil {
+		return err
+	}
+
+	*dst = opt.Some(res)
 	return nil
 }
