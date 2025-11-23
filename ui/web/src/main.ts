64268a75d2f42bf0881e4162c8c2df431ccbb43f
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-11-23 15:25:27 -0300

    feat(ui): update index.html and styles, and improve navigation

--- ui/web/src/main.ts
+++ ui/web/src/main.ts
@@ -1,4 +1,5 @@
 import { context, Orquestrator } from "./internal/context/context.ts"
+import Source from "./internal/support/source.ts"
 
 async function main(): Promise<void> {
     const sidebar = document.getElementById("sidebar")
@@ -6,68 +7,73 @@ async function main(): Promise<void> {
         throw new Error("There must be a #sidebar element")
     }
 
-    const content = document.getElementById("content")
-    if (content === null) {
-        throw new Error("There must be a #content element")
+    const main = document.getElementById("main")
+    if (main === null) {
+        throw new Error("There must be a #main element")
     }
 
-    const namespacing = setup_menu(content, "home", "users", "users-2", "about")
-    setup_sidebar(sidebar, namespacing)
+    setup_navigation(sidebar, main, "home", "users", "users-2", "about")
 }
 
-function setup_menu(room: HTMLElement, ...namespaces: string[]): (namespace: string) => boolean {
+function setup_navigation(sidebar: HTMLElement, room: HTMLElement, ...namespaces: string[]): void {
     if (namespaces.length === 0) {
         throw new Error("No namespaces are not allowed")
     }
+    
+    const progress = document.getElementById("progress")
 
     const orq = new Orquestrator(room)
-
     for (const namespace of namespaces) {
-        orq.Link(namespace, new context(`./pages/${namespace}.html`))
-    }
-
-    let hash = location.hash.slice(1)
-    if (!namespaces.includes(hash)) {
-        hash = namespaces[0]
-    }
-
-    if (orq.SwapTo(hash)) {
-        const target = document.querySelector(`[data-namespace="${hash}"]`)
-        if (target !== null) {
-            target.classList.add("selected")
-            location.hash = hash
-        }
-    }
-
-    return function (namespace: string): boolean {
-        if (orq.SwapTo(namespace)) {
-            location.hash = namespace
-            return true
+        const ctx = new context(Source.From(`/dist/pages/${namespace}.html`))
+        
+        if (progress !== null) {   
+            ctx.onpreload = () => { progress.classList.remove("complete") }
+            ctx.onload = () => { progress.classList.add("complete") }
         }
 
-        return false
+        orq.Link(namespace, ctx)
     }
-}
 
-function setup_sidebar(sidebar: HTMLElement, namespacing: (namespace: string) => boolean): void {
+    let current = orq.SwapperCurrent()
+    if (current === undefined || !namespaces.includes(current)) {
+        current = namespaces[0]
+    }
+
     const options = sidebar.querySelectorAll<HTMLElement>(".option")
     for (let i = 0; i < options.length; i++) {
         const option = options[i]
 
-        option.addEventListener("click", function (evt: MouseEvent) {
-            for (let j = 0; j < options.length; j++) {
-                options[j].classList.remove("selected")
-            }
-
-            const namespace = option.dataset["namespace"]
-            if (namespace !== undefined && namespacing(namespace)) {
-                option.scrollIntoView()
-                option.classList.add("selected")
-            }
-        })
-
+        option.addEventListener("click", handler_sidebar_click.bind(null, option, options, orq))
         click_for_keys(option, "Enter", " ")
     }
+
+    for (let i = 0; i < options.length; i++) {
+        const option = options[i]
+
+        const namespace = option.dataset["namespace"]
+        if (namespace === current) {
+            handler_sidebar_click(option, options, orq)
+            break
+        }
+    }
+}
+
+function handler_sidebar_click(option: HTMLElement, options: NodeListOf<HTMLElement>, orq: Orquestrator): void {
+    for (let j = 0; j < options.length; j++) {
+        options[j].classList.remove("selected")
+    }
+
+    const namespace = option.dataset["namespace"]
+    if (namespace === undefined) {
+        return
+    }
+
+    if (!orq.SwapTo(namespace)) {
+        return
+    }
+
+    option.classList.add("selected")
+    option.scrollIntoView()
 }
 
 function click_for_keys(element: HTMLElement, ...keys: string[]): void {
