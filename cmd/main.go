d025213bfa466f5d35e1e9be20fb281c6b8ded55
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-11-26 12:29:06 -0300

    feat(traffic): move traffic logging to middleware

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"context"
-	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"os"
@@ -16,24 +13,20 @@ import (
 
 	"github.com/alan-b-lima/almodon/internal/api/v1"
 	"github.com/alan-b-lima/almodon/internal/support/middleware"
-
-	"github.com/alan-b-lima/ansi-escape-sequences"
 )
 
 var StdOut = os.Stdout
 
 func main() {
 	log := middleware.NewLogger(StdOut, "")
-	style := Styles()
+	style := middleware.Styles()
 
 	ln, err := net.Listen("tcp", ":4545")
 	if err != nil {
 		log.Error(err)
 		return
 	}
-
-	url := "http://" + strings.Replace(ln.Addr().String(), "[::]", "localhost", 1)
-	log.Infof("Server listening at %s\n", style.HyperLink(url))
+	defer ln.Close()
 
 	api, err := api.New()
 	if err != nil {
@@ -41,18 +34,15 @@ func main() {
 		return
 	}
 	defer api.Close()
-
-	var mux http.ServeMux
-	mux.Handle("/", http.FileServer(http.Dir("../ui/web/")))
-	mux.Handle("/api/", api)
-	mux.HandleFunc("/terminate/{timeout}", Terminate)
-
-	srv := http.Server{Handler: TrafficLogMiddleware(log, style, &mux)}
-
+	
+	srv := http.Server{Handler: middleware.LogTraffic(log, style, MakeMux(api))}
 	done := EnableGracefulShutdown(func() {
 		log.Info("Shutting server down...")
 		srv.Shutdown(context.Background())
 	})
+
+	url := "http://" + strings.Replace(ln.Addr().String(), "[::]", "localhost", 1)
+	log.Infof("Server listening at %s\n", style.HyperLink(url))
 
 	if err := srv.Serve(ln); err != nil && err != http.ErrServerClosed {
 		log.Error(err)
@@ -61,26 +51,15 @@ func main() {
 	<-done
 }
 
-func TrafficLogMiddleware(log *middleware.Logger, s Style, handler http.Handler) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		rw := middleware.NewW(w)
+func MakeMux(api *api.Handler) *http.ServeMux {
+	mux := new(http.ServeMux)
 
-		handler.ServeHTTP(rw, r)
+	mux.Handle("/", http.FileServer(http.Dir("../ui/web/")))
+	mux.Handle("/dist/{path}", ServeFile("../ui/web/dist/index.html"))
+	mux.Handle("/api/", api)
+	mux.HandleFunc("/terminate/{timeout}", Terminate)
 
-		status := rw.StatusCode()
-		pen := s.StatusCodePen(status)
-
-		var barr [32]byte
-		b := bytes.NewBuffer(barr[:0])
-		pen.Writer = b
-
-		io.WriteString(&pen, fmt.Sprintf(" %03d ", status))
-		if 500 <= status && status < 599 {
-			log.Errorf("%s %s %s %s\n", b.String(), r.RemoteAddr, r.Method, r.URL)
-		} else {
-			log.Infof("%s %s %s %s\n", b.String(), r.RemoteAddr, r.Method, r.URL)
-		}
-	}
+	return mux
 }
 
 var Signals chan<- os.Signal
@@ -101,68 +80,10 @@ func EnableGracefulShutdown(fn func()) <-chan struct{} {
 	return done
 }
 
-type Style struct {
-	HyperLink func(string) string
+type ServeFile string
 
-	Pens    map[int]ansi.Pen
-	NoStyle ansi.Pen
-
-	Enabled bool
-}
-
-func Styles() (s Style) {
-	s.Enabled = ansi.EnableVirtualTerminal(os.Stdout.Fd()) == nil
-	s.NoStyle.SetStyle(false)
-
-	if s.Enabled {
-		var Success ansi.Pen
-		var Redirect ansi.Pen
-		var ClientError ansi.Pen
-		var ServerError ansi.Pen
-
-		Success.BGColor(ansi.RGBFromHex(0x0ed145))
-		Success.FGColor(ansi.RGBFromHex(0xffffff))
-
-		Redirect.BGColor(ansi.RGBFromHex(0x4b53cc))
-		Redirect.FGColor(ansi.RGBFromHex(0xffffff))
-
-		ClientError.BGColor(ansi.RGBFromHex(0xea1d1d))
-		ClientError.FGColor(ansi.RGBFromHex(0xffffff))
-
-		ServerError.BGColor(ansi.RGBFromHex(0x88001b))
-		ServerError.FGColor(ansi.RGBFromHex(0xffffff))
-
-		s.Pens = map[int]ansi.Pen{
-			2: Success,
-			3: Redirect,
-			4: ClientError,
-			5: ServerError,
-		}
-		s.HyperLink = hyperlink
-
-		return s
-	}
-
-	s.HyperLink = func(s string) string { return s }
-	s.Pens = map[int]ansi.Pen{}
-
-	return s
-}
-
-func (s *Style) StatusCodePen(status int) ansi.Pen {
-	pen, in := s.Pens[status/100]
-	if !in {
-		return s.NoStyle
-	}
-
-	return pen
-}
-
-func hyperlink(link string) string {
-	var pen ansi.Pen
-	pen.FGColor(ansi.RGBFromHex(0x4e8597))
-
-	return pen.Sprint(ansi.HyperLinkP(link))
+func (s ServeFile) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, string(s))
 }
 
 func Terminate(w http.ResponseWriter, r *http.Request) {
