a7e89b5bbd109c311bb7cf39a840849a42d7ac87
Breno A. Braga <emailbrenoaugusto@gmail.com>
2025-11-26 16:39:06 -0300

    fix(material): adjust naming

--- internal/domain/material/service.go
+++ internal/domain/material/service.go
@@ -8,9 +8,9 @@ type Service interface {
 	List(act auth.Actor, req ListRequest) (ListResponse, error)
 
 	Get(act auth.Actor, req GetRequest) (Response, error)
-	GetBySIADS(act auth.Actor, req GetBySIADSRequest) (Response, error)
-	GetByCATMAT(act auth.Actor, req GetByCATMATRequest) (Response, error)
-	GetByECAMPUS(act auth.Actor, req GetByECAMPUSRequest) (Response, error)
+	ListBySIADS(act auth.Actor, req ListBySIADSRequest) (Response, error)
+	ListByCATMAT(act auth.Actor, req ListByCATMATRequest) (Response, error)
+	ListByECampus(act auth.Actor, req ListByECampusRequest) (Response, error)
 
 	Create(act auth.Actor, req CreateRequest) (uuid.UUID, error)
 
@@ -18,6 +18,4 @@ type Service interface {
 	UpdateMinQuantity(act auth.Actor, req UpdateMinQuantityRequest) error
 
 	Delete(act auth.Actor, req DeleteRequest) error
-
-	Search(act auth.Actor, req SearchRequest) (ListResponse, error)
 }
