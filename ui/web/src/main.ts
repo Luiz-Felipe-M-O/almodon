927c4d8304f419ae50e18ae0ebfa77a47162b6a6
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2025-12-01 11:19:32 -0300

    feat(profile): integrate ProfileView and enhance user effect handling

--- ui/web/src/main.ts
+++ ui/web/src/main.ts
@@ -1,8 +1,15 @@
+import { Construct } from "./internal/api.ts"
+import { Admin, Chief, Promoted, Role, Unlogged, User } from "./internal/auth/auth.ts"
+import { ClickForKeys, ListenClickAndKeys } from "./internal/component/common.ts"
 import { Context, Orquestrator, resouce, StaticPage } from "./internal/context/context.ts"
 import UserView from "./internal/domain/user/view.ts"
+import ProfileView from "./internal/pages/profile.ts"
 import Source from "./internal/support/source.ts"
+import Signal from "./module/jsxmm/signals.ts"
 
 async function main(): Promise<void> {
+    const api = await Construct()
+
     const sidebar = document.getElementById("sidebar")
     if (sidebar === null) {
         throw new Error("There must be a #sidebar element")
@@ -13,38 +20,38 @@ async function main(): Promise<void> {
         throw new Error("There must be a #main element")
     }
 
-    const api = await setup_api()
+    const profile = new ProfileView(api.Users)
+    const user = profile.User()
 
-    setup_navigation(sidebar, main,
-        ["home", new StaticPage(Source.From("./dist/pages/home.html"))],
-        ["users", new UserView(api.users)],
-        ["table", new StaticPage(Source.From("./dist/pages/table.html"))],
-        ["about", new StaticPage(Source.From("./dist/pages/about.html"))],
+    const profiles = document.querySelectorAll<HTMLElement>(".profile .name")
+    for (let i = 0; i < profiles.length; i++) {
+        new Signal.Effect(effect_user.bind(null, profiles[i], user))
+    }
+
+    profile.HTML()
+
+    setup_navigation(sidebar, main, user,
+        ["home", new StaticPage(Source.From("./pages/home.html"))],
+        ["users", new UserView(api.Users)],
+        ["about", new StaticPage(Source.From("./pages/about.html"))],
+        ["profile", profile],
     )
 }
 
-async function setup_api(): Promise<{ users: user.Gateway }> {
-    if (Source.server === "") {
-        const users = await import("./internal/domain/user/gateway/mock.ts")
-        return {
-            users: new users.UserGateway(),
-        }
-    }
-
-    const users = await import("./internal/domain/user/gateway/api.ts")
-    return {
-        users: new users.UserGateway(Source.From("./users", Source.server)),
-    }
-}
-
-function setup_navigation(sidebar: HTMLElement, room: HTMLElement, ...namespaces: [string, Context][]): void {
+function setup_navigation(sidebar: HTMLElement, room: HTMLElement, user: Signal.RValue<user.Response | null>, ...namespaces: [string, Context][]): void {
     if (namespaces.length === 0) {
         throw new Error("No namespaces are not allowed")
     }
 
     const progress = document.getElementById("progress")
 
-    const orq = new Orquestrator(room)
+    let orq: Orquestrator
+    if (Source.server === "") {
+        orq = new Orquestrator(room)
+    } else {
+        orq = new Orquestrator(room, new resouce())
+    }
+
     for (const [namespace, context] of namespaces) {
         if (progress !== null && context instanceof StaticPage) {
             context.onpreload = () => { progress.classList.remove("complete") }
@@ -63,8 +70,7 @@ function setup_navigation(sidebar: HTMLElement, room: HTMLElement, ...namespaces
     for (let i = 0; i < options.length; i++) {
         const option = options[i]
 
-        option.addEventListener("click", handler_sidebar_click.bind(null, option, options, orq, progress))
-        click_for_keys(option, "Enter", " ")
+        ListenClickAndKeys(option, handler_sidebar_click.bind(null, option, options, orq, progress), "Enter", " ")
     }
 
     for (let i = 0; i < options.length; i++) {
@@ -73,9 +79,10 @@ function setup_navigation(sidebar: HTMLElement, room: HTMLElement, ...namespaces
         const namespace = option.dataset["namespace"]
         if (namespace === current) {
             handler_sidebar_click(option, options, orq, progress)
-            break
         }
     }
+
+    new Signal.Effect(effect_namespace.bind(null, options, orq, user))
 }
 
 function handler_sidebar_click(option: HTMLElement, options: NodeListOf<HTMLElement>, orq: Orquestrator, progress: HTMLElement | null): void {
@@ -100,13 +107,73 @@ function handler_sidebar_click(option: HTMLElement, options: NodeListOf<HTMLElem
     option.scrollIntoView()
 }
 
-function click_for_keys(element: HTMLElement, ...keys: string[]): void {
-    element.addEventListener("keydown", function (evt: KeyboardEvent) {
-        if (keys.includes(evt.key)) {
-            (evt.target as HTMLElement).click()
-            evt.preventDefault()
+function effect_user(profile: HTMLElement, signal: Signal.RValue<user.Response | null>): void {
+    const user = signal.Read()
+    if (user === null) {
+        profile.textContent = "Entrar"
+        return
+    }
+
+    const names = user.name.split(" ")
+    switch (names.length) {
+    case 0:
+        profile.textContent = "..."
+        break
+
+    case 1:
+        profile.textContent = names[0]
+        break
+
+    case 2:
+        profile.textContent = names[0] + " " + names[1]
+        break
+
+    default:
+        if (names[names.length - 2].length <= 3) {
+            profile.textContent = names[0] + " " + names[names.length - 2] + " " + names[names.length - 1]
+        } else {
+            profile.textContent = names[0] + " " + names[names.length - 1]
         }
-    })
+
+        break
+    }
 }
 
-window.addEventListener("DOMContentLoaded", main)
+const NamespaceForRoles: Record<Role, string[]> = {
+    [Chief]: ["home", "users", "about", "profile"],
+    [Promoted]: ["home", "about", "profile"],
+    [Admin]: ["home", "about", "profile"],
+    [User]: ["home", "about", "profile"],
+    [Unlogged]: ["home", "about", "profile"],
+}
+
+function effect_namespace(options: NodeListOf<HTMLElement>, orq: Orquestrator, signal: Signal.RValue<user.Response | null>): void {
+    const user = signal.Read()
+    let namespaces
+
+    if (user === null) {
+        namespaces = NamespaceForRoles[Unlogged]
+    } else {
+        namespaces = NamespaceForRoles[user.role as Role]
+    }
+
+    for (let i = 0; i < options.length; i++) {
+        const option = options[i]
+        option.style.display = "none"
+
+        const namespace = option.dataset["namespace"]
+        if (namespace === undefined) {
+            continue
+        }
+
+        if (namespaces.includes(namespace)) {
+            option.style.display = ""
+        }
+    }
+
+    if (orq.Current() === undefined || !namespaces.includes(orq.Current()!)) {
+        orq.SwapTo(namespaces[0])
+    }
+}
+
+window.addEventListener("DOMContentLoaded", main, { once: true })
