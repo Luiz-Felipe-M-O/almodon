8bcf7cac86cbd0f100e55fc24c24234ef17a0e75
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2026-02-27 23:30:16 -0300

    refector(opt)!: rename SomeThen to SomeSet to avoid confusion

--- internal/support/repository/opt.go
+++ internal/support/repository/opt.go
@@ -2,7 +2,7 @@ package repository
 
 import "github.com/alan-b-lima/pkg/opt"
 
-func SomeThen[T any](dst *T, src opt.Opt[T]) {
+func SomeSet[T any](dst *T, src opt.Opt[T]) {
 	val, ok := src.Unwrap()
 	if !ok {
 		return
