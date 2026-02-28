aa96836b512e5e8108298838671ad7657dd5e14d
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2026-02-27 23:29:26 -0300

    feat(index)!: narrow indexes

--- internal/support/repository/index.go
+++ internal/support/repository/index.go
@@ -1,32 +1,32 @@
 package repository
 
-type Index[K, V comparable] map[K]V
+type Index[K comparable] map[K]int
 
-func (i Index[K, V]) Get(key K) (V, bool) {
+func (i Index[K]) Get(key K) (int, bool) {
 	val, ok := i[key]
 	return val, ok
 }
 
-func (i Index[K, V]) Set(key K, value V) {
-	i[key] = value
+func (i Index[K]) Set(key K, index int) {
+	i[key] = index
 }
 
-func (i Index[K, V]) Del(key K) {
+func (i Index[K]) Del(key K) {
 	delete(i, key)
 }
 
-type MultiIndex[K, V comparable] map[K][]V
+type MultiIndex[K comparable] map[K][]int
 
-func (s MultiIndex[K, V]) Get(key K) ([]V, bool) {
+func (s MultiIndex[K]) Get(key K) ([]int, bool) {
 	values, ok := s[key]
 	return values, ok
 }
 
-func (s MultiIndex[K, V]) Add(key K, value V) {
-	s[key] = append(s[key], value)
+func (s MultiIndex[K]) Add(key K, index int) {
+	s[key] = append(s[key], index)
 }
 
-func (s MultiIndex[K, V]) Swap(key K, old, new V) {
+func (s MultiIndex[K]) Swap(key K, old, new int) {
 	values, ok := s[key]
 	if !ok {
 		return
@@ -40,7 +40,7 @@ func (s MultiIndex[K, V]) Swap(key K, old, new V) {
 	}
 }
 
-func (s MultiIndex[K, V]) Del(key K, value V) {
+func (s MultiIndex[K]) Del(key K, value int) {
 	values, ok := s[key]
 	if !ok {
 		return
