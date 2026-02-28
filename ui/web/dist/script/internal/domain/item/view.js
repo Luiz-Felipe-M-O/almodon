57e554de34c80c4e88dca44e5b2060311955230c
Alan Lima <145388354+AlanLima287@users.noreply.github.com>
2026-02-27 22:13:43 -0300

    feat(ui/web)!: get rid of frontend

--- ui/web/dist/script/internal/domain/item/view.js
+++
@@ -1,2 +0,0 @@
-"use strict";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidmlldy5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uLy4uL3NyYy9pbnRlcm5hbC9kb21haW4vaXRlbS92aWV3LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
