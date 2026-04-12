a794f2f9ba8141a2477d27815e9588d45a1aa820
Alan Lima <alanlimanvc@gmail.com>
2026-04-11 22:51:05 -0300

    refactor(middleware)!: simplify ResponseWriteWithStatus structure

--- internal/support/middleware/status.go
+++ internal/support/middleware/status.go
@@ -1,56 +1,25 @@
 package middleware
 
-import (
-	"errors"
-	"net/http"
-)
+import "net/http"
 
-type ResponseWriter struct {
-	w     http.ResponseWriter
+type ResponseWriterWithStatus struct {
+	http.ResponseWriter
 	status int
 }
 
-func NewW(w http.ResponseWriter) *ResponseWriter {
-	return &ResponseWriter{w: w, status: 200}
+func NewResponseWriterWithStatus(w http.ResponseWriter) *ResponseWriterWithStatus {
+	return &ResponseWriterWithStatus{ResponseWriter: w, status: 200}
 }
 
-func (rw *ResponseWriter) WriteHeader(status int) {
-	rw.w.WriteHeader(status)
+var _ http.ResponseWriter = (*ResponseWriterWithStatus)(nil)
+
+func (rw *ResponseWriterWithStatus) WriteHeader(status int) {
+	rw.ResponseWriter.WriteHeader(status)
 	rw.status = status
 }
 
-func (rw *ResponseWriter) Header() http.Header           { return rw.w.Header() }
-func (rw *ResponseWriter) Write(buf []byte) (int, error) { return rw.w.Write(buf) }
-func (rw *ResponseWriter) StatusCode() int               { return rw.status }
+func (rw *ResponseWriterWithStatus) StatusCode() int { return rw.status }
 
-type ResponseWriterFlusher struct {
-	wf interface {
-		http.ResponseWriter
-		http.Flusher
-	}
-	status int
+func (rw *ResponseWriterWithStatus) Unwrap() http.ResponseWriter {
+	return rw.ResponseWriter
 }
-
-var errNoFlush = errors.New("flushing is not supported")
-
-func NewWF(w http.ResponseWriter) (*ResponseWriterFlusher, error) {
-	wf, ok := w.(interface {
-		http.ResponseWriter
-		http.Flusher
-	})
-	if !ok {
-		return nil, errNoFlush
-	}
-
-	return &ResponseWriterFlusher{wf: wf, status: 200}, nil
-}
-
-
-func (rw *ResponseWriterFlusher) WriteHeader(status int) {
-	rw.wf.WriteHeader(status)
-	rw.status = status
-}
-
-func (rw *ResponseWriterFlusher) Header() http.Header           { return rw.wf.Header() }
-func (rw *ResponseWriterFlusher) Write(buf []byte) (int, error) { return rw.wf.Write(buf) }
-func (rw *ResponseWriterFlusher) Flush()                        { rw.wf.Flush() }
