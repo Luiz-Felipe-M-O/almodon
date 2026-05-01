eeec8ea4bddf521a08b9c1f1ca10d6b54658c4be
Alan Lima <alanlimanvc@gmail.com>
2026-05-01 02:07:14 -0300

    feat(api, domain): split initialization between api and domain

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,259 +1,43 @@
 package api
 
 import (
-	"context"
-	"database/sql"
-	"errors"
 	"net/http"
-	"os"
 
-	"github.com/alan-b-lima/almodon/internal/domain/auth"
-	auths "github.com/alan-b-lima/almodon/internal/domain/auth/resource"
-	authserve "github.com/alan-b-lima/almodon/internal/domain/auth/service"
-
-	"github.com/alan-b-lima/almodon/internal/domain/item"
-	items "github.com/alan-b-lima/almodon/internal/domain/item/resource"
-	itemserve "github.com/alan-b-lima/almodon/internal/domain/item/service"
-	itemstore "github.com/alan-b-lima/almodon/internal/domain/item/store"
-
-	"github.com/alan-b-lima/almodon/internal/domain/material"
-	materials "github.com/alan-b-lima/almodon/internal/domain/material/resource"
-	materialserve "github.com/alan-b-lima/almodon/internal/domain/material/service"
-	materialstore "github.com/alan-b-lima/almodon/internal/domain/material/store"
-
-	"github.com/alan-b-lima/almodon/internal/domain/promotion"
-	promotions "github.com/alan-b-lima/almodon/internal/domain/promotion/resource"
-	promotionserve "github.com/alan-b-lima/almodon/internal/domain/promotion/service"
-	promotionstore "github.com/alan-b-lima/almodon/internal/domain/promotion/store"
-
-	"github.com/alan-b-lima/almodon/internal/domain/session"
-	sessionserve "github.com/alan-b-lima/almodon/internal/domain/session/service"
-	sessionstore "github.com/alan-b-lima/almodon/internal/domain/session/store"
-
-	"github.com/alan-b-lima/almodon/internal/domain/user"
-	users "github.com/alan-b-lima/almodon/internal/domain/user/resource"
-	userserve "github.com/alan-b-lima/almodon/internal/domain/user/service"
-	userstore "github.com/alan-b-lima/almodon/internal/domain/user/store"
-
+	"github.com/alan-b-lima/almodon/internal/domain"
+	"github.com/alan-b-lima/almodon/internal/support/session"
 	"github.com/alan-b-lima/almodon/pkg/closer"
-
-	"github.com/alan-b-lima/pkg/scheduler"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 type Almodon struct {
-	http.ServeMux
+	http.Handler
 	bundle closer.Bundle
 }
 
-type (
-	Stores struct {
-		Items      item.Store
-		Materials  material.Store
-		Promotions promotion.Store
-		Sessions   session.Store
-		Users      user.Store
-	}
-
-	Services struct {
-		Auths      auth.Service
-		Items      item.Service
-		Materials  material.Service
-		Promotions promotion.Service
-		Sessions   session.Service
-		Users      user.Service
-	}
-
-	Resources struct {
-		Auth       *auths.Resource
-		Items      *items.Resource
-		Materials  *materials.Resource
-		Promotions *promotions.Resource
-		Users      *users.Resource
-	}
-)
-
-var ErrNoRootUser = errors.New("no root user found in database")
-
 func New() (*Almodon, error) {
-	var a Almodon
-	var err error
-
-	defer func(err *error) {
-		if *err != nil {
-			a.Close()
-		}
-	}(&err)
-
-	ctx := context.TODO()
-
-	db, err := a.MountSQLiteDB(ctx)
+	domain, err := domain.New()
 	if err != nil {
 		return nil, err
 	}
-
-	stores, err := a.MountSQLiteStores(ctx, db)
-	if err != nil {
-		return nil, err
-	}
-
-	core, err := a.MountServices(ctx, stores)
-	if err != nil {
-		return nil, err
-	}
-
-	services := a.MountAuthServices(core)
-	resources := a.MountResources(services)
 
 	handlers := map[string]http.Handler{
-		"auth":       resources.Auth,
-		"items":      resources.Items,
-		"materials":  resources.Materials,
-		"promotions": resources.Promotions,
-		"users":      resources.Users,
+		"auth":       domain.Resources.Auth,
+		"items":      domain.Resources.Items,
+		"materials":  domain.Resources.Materials,
+		"promotions": domain.Resources.Promotions,
+		"users":      domain.Resources.Users,
 	}
+
+	mux := http.NewServeMux()
 	for name, handler := range handlers {
-		a.Handle("/api/v1/"+name+"/", http.StripPrefix("/api/v1", handler))
+		mux.Handle("/api/v1/"+name+"/", http.StripPrefix("/api/v1", handler))
 	}
 
-	return &a, nil
-}
-
-func (a *Almodon) MountSQLiteDB(ctx context.Context) (*sql.DB, error) {
-	var db *sql.DB
-
-	_, err := os.Stat(".data/almodon.db")
-	if !errors.Is(err, os.ErrNotExist) {
-		db, err = sql.Open("sqlite3", ".data/almodon.db")
-	} else {
-		db, err = sql.Open("sqlite3", "../.data/almodon.db")
-	}
-	if err != nil {
-		return nil, err
-	}
-	a.bundle.Bundle(db)
-
-	if err := db.PingContext(ctx); err != nil {
-		return nil, err
-	}
-
-	operations := [...]string{
-		"PRAGMA foreign_keys = ON;",
-
-		materialstore.Table,
-		itemstore.Table,
-		userstore.Table,
-		sessionstore.Table,
-		promotionstore.Table,
-
-		materialstore.Indexes,
-		promotionstore.Indexes,
-		sessionstore.Indexes,
-		userstore.Indexes,
-
-		itemstore.Views,
-		userstore.Views,
-	}
-
-	for _, op := range operations {
-		if _, err := db.ExecContext(ctx, op); err != nil {
-			return nil, err
-		}
-	}
-
-	return db, nil
-}
-
-func (a *Almodon) MountSQLiteStores(ctx context.Context, db *sql.DB) (Stores, error) {
-	stores := Stores{
-		Items:      itemstore.New(db),
-		Materials:  materialstore.New(db),
-		Promotions: promotionstore.New(db),
-		Sessions:   sessionstore.New(db),
-		Users:      userstore.New(db),
-	}
-
-	if err := has_root_user(ctx, stores.Users); err != nil {
-		return Stores{}, err
-	}
-
-	return stores, nil
-}
-
-func (a *Almodon) MountServices(ctx context.Context, stores Stores) (Services, error) {
-	scheduler := scheduler.New()
-	a.bundle.BundleFunc(scheduler.Stop)
-	scheduler.Start()
-
-	services := Services{
-		Items:     itemserve.New(stores.Items),
-		Materials: materialserve.New(stores.Materials),
-		Sessions:  sessionserve.New(stores.Sessions, scheduler),
-		Users:     userserve.New(stores.Users),
-	}
-
-	services.Auths = authserve.New(services.Users, services.Sessions)
-	services.Promotions = promotionserve.New(stores.Promotions, services.Users, scheduler)
-
-	var (
-		err_promotions = post_for_scheduler(ctx, services.Promotions)
-		err_sessions   = post_for_scheduler(ctx, services.Sessions)
-	)
-	if err := errors.Join(err_sessions, err_promotions); err != nil {
-		return Services{}, err
-	}
-
-	return services, nil
-}
-
-func (a *Almodon) MountAuthServices(services Services) Services {
-	authed := Services{
-		Auths:      services.Auths,
-		Items:      itemserve.NewGate(services.Items, services.Auths),
-		Materials:  materialserve.NewGate(services.Materials, services.Auths),
-		Promotions: promotionserve.NewGate(services.Promotions, services.Auths),
-		Sessions:   services.Sessions,
-		Users:      userserve.NewGate(services.Users, services.Auths),
-	}
-
-	return authed
-}
-
-func (a *Almodon) MountResources(services Services) Resources {
-	resources := Resources{
-		Auth:       auths.New(services.Auths),
-		Items:      items.New(services.Items),
-		Materials:  materials.New(services.Materials),
-		Promotions: promotions.New(services.Promotions),
-		Users:      users.New(services.Users),
-	}
-
-	return resources
+	return &Almodon{
+		Handler: session.Wrap(mux),
+		bundle:  domain.Bundle,
+	}, nil
 }
 
 func (a *Almodon) Close() error {
 	return a.bundle.Close()
-}
-
-func has_root_user(ctx context.Context, store user.Store) error {
-	_, err := store.GetBySIAPE(ctx, "0000000")
-	if err != nil {
-		if err == user.ErrNotFound {
-			return ErrNoRootUser
-		}
-
-		return err
-	}
-
-	return nil
-}
-
-func post_for_scheduler(ctx context.Context, service any) error {
-	pub, ok := service.(interface{ Publish(context.Context) error })
-	if !ok {
-		return nil
-	}
-
-	return pub.Publish(ctx)
 }
---
+++ internal/domain/domain.go
@@ -0,0 +1,295 @@
+package domain
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"os"
+
+	"github.com/alan-b-lima/almodon/internal/domain/auth"
+	auths "github.com/alan-b-lima/almodon/internal/domain/auth/resource"
+	authserve "github.com/alan-b-lima/almodon/internal/domain/auth/service"
+
+	"github.com/alan-b-lima/almodon/internal/domain/item"
+	items "github.com/alan-b-lima/almodon/internal/domain/item/resource"
+	itemserve "github.com/alan-b-lima/almodon/internal/domain/item/service"
+	itemstore "github.com/alan-b-lima/almodon/internal/domain/item/store"
+
+	"github.com/alan-b-lima/almodon/internal/domain/material"
+	materials "github.com/alan-b-lima/almodon/internal/domain/material/resource"
+	materialserve "github.com/alan-b-lima/almodon/internal/domain/material/service"
+	materialstore "github.com/alan-b-lima/almodon/internal/domain/material/store"
+
+	"github.com/alan-b-lima/almodon/internal/domain/promotion"
+	promotions "github.com/alan-b-lima/almodon/internal/domain/promotion/resource"
+	promotionserve "github.com/alan-b-lima/almodon/internal/domain/promotion/service"
+	promotionstore "github.com/alan-b-lima/almodon/internal/domain/promotion/store"
+
+	"github.com/alan-b-lima/almodon/internal/domain/session"
+	sessionserve "github.com/alan-b-lima/almodon/internal/domain/session/service"
+	sessionstore "github.com/alan-b-lima/almodon/internal/domain/session/store"
+
+	"github.com/alan-b-lima/almodon/internal/domain/user"
+	users "github.com/alan-b-lima/almodon/internal/domain/user/resource"
+	userserve "github.com/alan-b-lima/almodon/internal/domain/user/service"
+	userstore "github.com/alan-b-lima/almodon/internal/domain/user/store"
+
+	"github.com/alan-b-lima/almodon/pkg/closer"
+	"github.com/alan-b-lima/pkg/scheduler"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Domain struct {
+	Database  *sql.DB
+	Scheduler *scheduler.Scheduler
+
+	Stores    Stores
+	Cores     Cores
+	Services  Services
+	Resources Resources
+
+	Bundle closer.Bundle
+}
+
+type (
+	Stores struct {
+		Items      item.Store
+		Materials  material.Store
+		Promotions promotion.Store
+		Sessions   session.Store
+		Users      user.Store
+	}
+
+	Cores struct {
+		Auths      *authserve.Core
+		Items      *itemserve.Core
+		Materials  *materialserve.Core
+		Promotions *promotionserve.Core
+		Sessions   *sessionserve.Core
+		Users      *userserve.Core
+	}
+
+	Services struct {
+		Auths      auth.Service
+		Items      item.Service
+		Materials  material.Service
+		Promotions promotion.Service
+		Sessions   session.Service
+		Users      user.Service
+	}
+
+	Resources struct {
+		Auth       *auths.Resource
+		Items      *items.Resource
+		Materials  *materials.Resource
+		Promotions *promotions.Resource
+		Users      *users.Resource
+	}
+)
+
+func New(opts ...Option) (*Domain, error) {
+	var bundle closer.Bundle
+	var err error
+
+	defer func(err *error) {
+		if err != nil {
+			bundle.Close()
+		}
+	}(&err)
+
+	db, err := MountSQLiteDB([]string{
+		".data/almodon.db",
+		"../.data/almodon.db",
+		"../../.data/almodon.db",
+	})
+	if err != nil {
+		return nil, err
+	}
+	bundle.Bundle(db)
+
+	scheduler := scheduler.New()
+	bundle.BundleFunc(scheduler.Stop)
+	scheduler.Start()
+
+	stores := MountStores(db)
+	cores := MountCores(scheduler, stores)
+	services := MountServices(cores)
+	resources := MountResources(services)
+
+	domain := Domain{
+		Database:  db,
+		Scheduler: scheduler,
+
+		Stores:    stores,
+		Cores:     cores,
+		Services:  services,
+		Resources: resources,
+
+		Bundle: bundle,
+	}
+
+	opt := Condense(opts...)
+
+	if opt&Structure != 0 {
+		if err = PrepareStructure(db); err != nil {
+			return nil, err
+		}
+	}
+
+	if opt&RootUser != 0 {
+		if err = HasRootUser(stores.Users); err != nil {
+			return nil, err
+		}
+	}
+
+	if opt&Publish != 0 {
+		if err = PublishForScheduler(cores.Sessions, cores.Promotions); err != nil {
+			return nil, err
+		}
+	}
+
+	return &domain, nil
+}
+
+type Option int
+
+const (
+	Structure Option = 1 << iota
+	RootUser
+	Publish
+
+	Default = Structure | RootUser | Publish
+)
+
+func Condense(opts ...Option) Option {
+	if len(opts) == 0 {
+		return Default
+	}
+
+	var final Option
+	for _, opt := range opts {
+		final |= opt
+	}
+
+	return final
+}
+
+func MountSQLiteDB(names []string) (*sql.DB, error) {
+	var name string
+	for _, n := range names {
+		_, err := os.Stat(n)
+		if !errors.Is(err, os.ErrNotExist) {
+			name = n
+			break
+		}
+	}
+	if name == "" {
+		return nil, os.ErrNotExist
+	}
+
+	return sql.Open("sqlite3", name)
+}
+
+func MountStores(db *sql.DB) Stores {
+	stores := Stores{
+		Items:      itemstore.New(db),
+		Materials:  materialstore.New(db),
+		Promotions: promotionstore.New(db),
+		Sessions:   sessionstore.New(db),
+		Users:      userstore.New(db),
+	}
+
+	return stores
+}
+
+func MountCores(scheduler *scheduler.Scheduler, stores Stores) Cores {
+	cores := Cores{
+		Items:     itemserve.New(stores.Items),
+		Materials: materialserve.New(stores.Materials),
+		Sessions:  sessionserve.New(stores.Sessions, scheduler),
+		Users:     userserve.New(stores.Users),
+	}
+
+	cores.Auths = authserve.New(cores.Users, cores.Sessions)
+	cores.Promotions = promotionserve.New(stores.Promotions, cores.Users, scheduler)
+
+	return cores
+}
+
+func MountServices(cores Cores) Services {
+	authed := Services{
+		Auths:      cores.Auths,
+		Items:      itemserve.NewGate(cores.Items, cores.Auths),
+		Materials:  materialserve.NewGate(cores.Materials, cores.Auths),
+		Promotions: promotionserve.NewGate(cores.Promotions, cores.Auths),
+		Sessions:   cores.Sessions,
+		Users:      userserve.NewGate(cores.Users, cores.Auths),
+	}
+
+	return authed
+}
+
+func MountResources(services Services) Resources {
+	resources := Resources{
+		Auth:       auths.New(services.Auths),
+		Items:      items.New(services.Items),
+		Materials:  materials.New(services.Materials),
+		Promotions: promotions.New(services.Promotions),
+		Users:      users.New(services.Users),
+	}
+
+	return resources
+}
+
+var ErrNoRootUser = errors.New("no root user found in database")
+
+var operations = [...]string{
+	"PRAGMA foreign_keys = ON;",
+
+	materialstore.Table,
+	itemstore.Table,
+	userstore.Table,
+	sessionstore.Table,
+	promotionstore.Table,
+
+	materialstore.Indexes,
+	promotionstore.Indexes,
+	sessionstore.Indexes,
+	userstore.Indexes,
+
+	itemstore.Views,
+	userstore.Views,
+}
+
+func PrepareStructure(db *sql.DB) error {
+	for _, op := range operations {
+		if _, err := db.Exec(op); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func HasRootUser(users user.Store) error {
+	ctx := context.TODO()
+
+	_, err := users.GetBySIAPE(ctx, "0000000")
+	if err == user.ErrNotFound {
+		return ErrNoRootUser
+	}
+
+	return err
+}
+
+func PublishForScheduler(sessions *sessionserve.Core, promotions *promotionserve.Core) error {
+	ctx := context.TODO()
+
+	var (
+		err0 = sessions.Publish(ctx)
+		err1 = promotions.Publish(ctx)
+	)
+
+	return errors.Join(err0, err1)
+}
