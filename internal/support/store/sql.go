37e932ffdc022793267793ca384fd5a405169d20
Alan Lima <alanlimanvc@gmail.com>
2026-05-09 14:07:08 -0300

    feat(store/sql): add primitives to support transaction extension

--- internal/support/store/sql.go
+++ internal/support/store/sql.go
@@ -12,8 +12,17 @@ type DBTx interface {
 	QueryRowContext(context.Context, string, ...any) *sql.Row
 }
 
+type Txer interface {
+	Tx() DBTx
+}
+
 type Scanner interface {
 	Scan(...any) error
+}
+
+type txconn struct {
+	*sql.Tx
+	pool *sql.DB
 }
 
 func WithTx(ctx context.Context, dbtx DBTx, proc func(DBTx) error) error {
@@ -27,7 +36,7 @@ func WithTx(ctx context.Context, dbtx DBTx, proc func(DBTx) error) error {
 		return ErrTx.Cause(err).Make()
 	}
 
-	if err := proc(tx); err != nil {
+	if err := proc(&txconn{Tx: tx, pool: db}); err != nil {
 		if err := tx.Rollback(); err != nil {
 			return ErrTx.Cause(err).Make()
 		}
@@ -38,4 +47,27 @@ func WithTx(ctx context.Context, dbtx DBTx, proc func(DBTx) error) error {
 		return ErrTx.Cause(err).Make()
 	}
 	return nil
+}
+
+func JoinTx(txed any, joiner DBTx) (DBTx, error) {
+	txer, ok := txed.(Txer)
+	if !ok {
+		return nil, ErrNotExtendable
+	}
+
+	tx, ok := txer.Tx().(*txconn)
+	if !ok {
+		return nil, ErrNotTx
+	}
+
+	db, ok := joiner.(*sql.DB)
+	if !ok {
+		return nil, ErrNestedTx
+	}
+
+	if db != tx.pool {
+		return nil, ErrIllegalJoin
+	}
+
+	return tx, nil
 }
--- internal/support/store/xerrors.go
+++ internal/support/store/xerrors.go
@@ -3,11 +3,15 @@ package store
 import "github.com/alan-b-lima/pkg/problem"
 
 var (
-	ErrDB = problem.Imp(problem.UnexpectedError, "database-error").Message("unknown error while accessing database")
+	ErrDB = problem.Imp(problem.UnexpectedError, "database-error").Message("unexpected error while accessing database")
 
-	ErrQuery = problem.Imp(problem.UnexpectedError, "query-error").Message("unknown error while executing query")
-	ErrExec  = problem.Imp(problem.UnexpectedError, "exec-error").Message("unknown error while executing command")
+	ErrQuery = problem.Imp(problem.UnexpectedError, "query-error").Message("unexpected error while executing query")
+	ErrExec  = problem.Imp(problem.UnexpectedError, "exec-error").Message("unexpected error while executing command")
 
-	ErrTx       = problem.Imp(problem.UnexpectedError, "transaction-error").Message("unknown error while processing transaction")
+	ErrTx       = problem.Imp(problem.UnexpectedError, "transaction-error").Message("unexpected error while processing transaction")
 	ErrNestedTx = problem.New(problem.UnexpectedError, "open-transaction-inside-transaction", "cannot open a transaction inside another transaction", nil, nil)
+
+	ErrNotExtendable = problem.New(problem.UnexpectedError, "not-extendable", "store does not support transaction extension", nil, nil)
+	ErrNotTx         = problem.New(problem.UnexpectedError, "not-transaction", "store is not a transaction", nil, nil)
+	ErrIllegalJoin   = problem.New(problem.UnexpectedError, "illegal-join", "cannot join transactions from different pools", nil, nil)
 )
